4080e6337b09193663de5b5849efe444793c3db4
Nohier <nohierassan2@gmail.com>
2020-04-23 19:28:50 +0200

    Testing Hooks 4

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World , Testing the custome Hooks3 !";
+    private final String message = "Hello World , Testing the custome Hooks4 !";
 
     public App() {}
 
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,9 +34,9 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello World , Testing the custome Hooks3 !" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello World , Testing the custome Hooks4 !" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World , Testing the custome Hooks3 !\"");
+            fail("\"message\" is not \"Hello World , Testing the custome Hooks4 !\"");
         }
     }
 
