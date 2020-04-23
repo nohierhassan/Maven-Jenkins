4026ffd89c33ececf4d878a4ae32e98deb1ef474
Nohier <nohierassan2@gmail.com>
2020-04-23 18:51:28 +0200

    Testing Custom-Hooks

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,9 +34,9 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello Jenkins!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello World , Testing the custome Hooks !" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello Jenkins!\"");
+            fail("\"message\" is not \"Hello World , Testing the custome Hooks !\"");
         }
     }
 
