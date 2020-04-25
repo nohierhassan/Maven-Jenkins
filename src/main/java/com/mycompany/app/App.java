dfc313d1bb63a1acc975240c77c974a92d7228c0
Nohier <nohierassan2@gmail.com>
2020-04-25 09:42:56 +0200

    Testing DSL-Job

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World , Testing the custome Hooks5 !";
+    private final String message = "Hello World , Testing the custome Hooks6 !";
 
     public App() {}
 
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,9 +34,9 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello World , Testing the custome Hooks5 !" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello World , Testing the custome Hooks6 !" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World , Testing the custome Hooks5 !\"");
+            fail("\"message\" is not \"Hello World , Testing the custome Hooks6 !\"");
         }
     }
 
